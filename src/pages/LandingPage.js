8dc2d8907e68e2dcd78b2f70c4a8350e15ce4873
unknown <redwoodridofu@gmail.com>
2020-09-29 03:03:27 +0700

    them footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,6 +4,8 @@ import Example from '../components/Nav';
 import Sec from '../components/sec_one';
 import '../scss/landingpage.scss'
 import Sectwo from '../components/sec_two';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExclamationTriangle, faUpload, faEnvelope, faPhone  } from '@fortawesome/free-solid-svg-icons'
 
 
 class LandingPage extends React.Component {
@@ -31,6 +33,69 @@ class LandingPage extends React.Component {
 
           </section>
         </main>
+        <section className='footer'>
+          <Container>
+            <Row className='footer-1'>
+              <Col sm='4' className='fooer-block'>
+                <div className='footer-header'>
+                  <h6>Giới thiệu</h6>
+                </div>
+                <div>
+                  <p className='footer-content'>
+                  Truyencv là website đọc truyện convert online cập nhật liên tục và nhanh nhất các truyện tiên hiệp, 
+                  kiếm hiệp, huyền ảo được các thành viên liên tục đóng góp rất nhiều truyện hay và nổi bật: The 99th Divorce
+                  </p>
+                </div>
+              </Col>
+              <Col sm='4' className='fooer-block'>
+                <div className='footer-header'>
+                  <h6>Links</h6>
+                </div>
+                <ul>
+                  <li>
+                    <div className='footer-icon-content'>
+                      <FontAwesomeIcon className='block-list-icon' icon={faExclamationTriangle}></FontAwesomeIcon>
+                      <p className='footer-content'>
+                        Privacy         
+                      </p>          
+                    </div>
+                  </li>
+                  <li>
+                    <div className='footer-icon-content'>
+                      <FontAwesomeIcon className='block-list-icon' icon={faUpload}></FontAwesomeIcon>
+                      <p className='footer-content'>
+                        FQA         
+                      </p>          
+                    </div>
+                  </li>
+                </ul>
+              </Col>
+              <Col sm='4' className='fooer-block'>
+                <div className='footer-header'>
+                  <h6>Liên hệ quảng cáo</h6>
+                </div>
+                <ul>
+                  <li>
+                    <div className='footer-icon-content'>
+                      <FontAwesomeIcon className='block-list-icon' icon={faEnvelope}></FontAwesomeIcon>
+                      <p className='footer-content'>
+                        Mail        
+                      </p>          
+                    </div>
+                  </li>
+                  <li>
+                    <div className='footer-icon-content'>
+                      <FontAwesomeIcon className='block-list-icon' icon={faPhone}></FontAwesomeIcon>
+                      <p className='footer-content'>
+                        Phone
+                      </p>          
+                    </div>
+                  </li>
+                </ul>
+              </Col>
+            </Row>
+          </Container>
+        </section>
        </div>
       );
     }
